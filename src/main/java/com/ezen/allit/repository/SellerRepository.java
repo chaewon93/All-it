963719819f83002ef7dbb09e3fbc6e912680f2e6
HyunIl <snutops@gamil.com>
2023-01-30 16:01:59 +0900

    0.0.3 commit park

--- src/main/java/com/ezen/allit/repository/SellerRepository.java
+++ src/main/java/com/ezen/allit/repository/SellerRepository.java
@@ -1,12 +1,20 @@
 package com.ezen.allit.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.ezen.allit.domain.Role;
 import com.ezen.allit.domain.Seller;
 
 public interface SellerRepository extends JpaRepository<Seller, String> {
 
 	// 판매자 로그인
 	Seller findByIdAndPwd(String id, String pwd);
-
+	
+	// 등록대기 판매자, 등록완료 판매자 조회
+	List<Seller> findSellerByRoleNot(Role role);
+	
+	// 관리자 조회
+	List<Seller> findSellerByRole(Role role);
 }
